9c299bb21e16041a4ca2854030464e8d1bffdb24
Preston <xu9CaB7821M!@z>
2018-09-29 21:44:16 -0600

    Version 0.6.26

--- server/menu.php
+++ server/menu.php
@@ -57,39 +57,41 @@
                 $menuspace = 1;
             }
         } else {
-            ?>
 
-            <div class="sm_menu_item<?php if ( $area == 0 ) {
-                echo ' sm_menu_active';
-            } ?>"><a href="?area=0">Status</a></div>
-            <div class="sm_menu_item<?php if ( $area == 2 ) {
-                echo ' sm_menu_active';
-            } ?>"><a href="?area=2">Single Backup</a></div>
-            <div class="sm_menu_item<?php if ( $area == 3 ) {
-                echo ' sm_menu_active';
-            } ?>"><a href="?area=3">Restore</a></div>
-
-            <?php
-            if ( ! empty( $settings['xen_ip'] ) ) {
+            if (! empty( $settings['drive_type'] )) {
                 ?>
-                <div class="sm_menu_item<?php if ( $area == 5 ) {
+
+                <div class="sm_menu_item<?php if ( $area == 0 ) {
                     echo ' sm_menu_active';
-                } ?>"><a href="?area=5">Xen Migrate</a></div>
+                } ?>"><a href="?area=0">Status</a></div>
+                <div class="sm_menu_item<?php if ( $area == 2 ) {
+                    echo ' sm_menu_active';
+                } ?>"><a href="?area=2">Single Backup</a></div>
+                <div class="sm_menu_item<?php if ( $area == 3 ) {
+                    echo ' sm_menu_active';
+                } ?>"><a href="?area=3">Restore</a></div>
+
+                <?php
+                if ( ! empty( $settings['xen_ip'] ) ) {
+                    ?>
+                    <div class="sm_menu_item<?php if ( $area == 5 ) {
+                        echo ' sm_menu_active';
+                    } ?>"><a href="?area=5">Xen Migrate</a></div>
+                    <?php
+                }
+
+                ?>
+                <div class="sm_menu_item<?php if ( $area == 10 ) {
+                    echo ' sm_menu_active';
+                } ?>"><a href="?area=10">Logs</a></div>
+
+
+                <div class="sm_menu_item<?php if ( $area == 1 ) {
+                    echo ' sm_menu_active';
+                } ?>"><a href="?area=1">Scheduled Backups</a></div>
+
                 <?php
             }
-
-            ?>
-            <div class="sm_menu_item<?php if ( $area == 10 ) {
-                echo ' sm_menu_active';
-            } ?>"><a href="?area=10">Logs</a></div>
-
-
-            <div class="sm_menu_item<?php if ( $area == 1 ) {
-                echo ' sm_menu_active';
-            } ?>"><a href="?area=1">Scheduled Backups</a></div>
-
-            <?php
-
         }
     ?>
 
